b7d6a7a22b585b8c79f5d283af6b685605b2ac7e
GregLuo-Downer <127060494+GregLuo-Downer@users.noreply.github.com>
2024-06-21 13:21:22 +1000

    TvPage

--- src/features/program/TvPage.tsx
+++ src/features/program/TvPage.tsx
@@ -1,10 +1,32 @@
+'use client';
+
+import { selectSelectedTv } from '@/lib/features/carousel/carouselSlice';
 import { Stack } from '@mui/material';
+import { useSelector } from 'react-redux';
+import Image from 'next/image';
 
 export default function TvPage() {
+  const selectedTv = useSelector(selectSelectedTv);
+
   return (
     <>
-      <Stack direction='row' spacing={2}>
-        fgf
+      <Stack direction='row' spacing={2} sx={{ background: 'black' }}>
+        <Image alt={selectedTv?.title} src={selectedTv?.image} title={selectedTv?.title} key={selectedTv?.id} width={250} height={250} />
+        <Stack direction='column' spacing={2} className='text-white'>
+          <div className='text-3xl'>{selectedTv?.title}</div>
+          <Stack direction='row' spacing={2}>
+            <div>{selectedTv?.rating}</div>
+            <span>|</span>
+            <div> {selectedTv?.year}</div>
+            <span>|</span>
+            <div>{selectedTv?.type}</div>
+            <span>|</span>
+            <div>{selectedTv?.genre}</div>
+            <span>|</span>
+            <div>{selectedTv?.language}</div>
+          </Stack>
+          <div>{selectedTv?.description}</div>
+        </Stack>
       </Stack>
     </>
   );
