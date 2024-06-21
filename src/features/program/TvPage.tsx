7b05780a131b1a7d5191ad1ef2188854fd13e101
GregLuo-Downer <127060494+GregLuo-Downer@users.noreply.github.com>
2024-06-21 14:29:28 +1000

    handleBack

--- src/features/program/TvPage.tsx
+++ src/features/program/TvPage.tsx
@@ -1,30 +1,41 @@
 'use client';
 
 import { selectSelectedTv } from '@/lib/features/carousel/carouselSlice';
-import { Stack } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 import { useSelector } from 'react-redux';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function TvPage() {
+  const router = useRouter();
   const selectedTv = useSelector(selectSelectedTv);
+
+  function handleBack() {
+    router.push('/carousel');
+  }
 
   return (
     <>
-      <Stack direction='row' spacing={2} sx={{ background: 'black' }}>
+      <Button variant='outlined' onClick={handleBack}>
+        Back
+      </Button>
+      <Stack direction='row' spacing={5} sx={{ background: 'black', margin: '15px' }}>
         <Image alt={selectedTv?.title} src={selectedTv?.image} title={selectedTv?.title} key={selectedTv?.id} width={250} height={250} />
-        <Stack direction='column' spacing={2} className='text-white'>
-          <div className='text-3xl'>{selectedTv?.title}</div>
-          <Stack direction='row' spacing={2}>
-            <div>{selectedTv?.rating}</div>
-            <span>|</span>
-            <div> {selectedTv?.year}</div>
-            <span>|</span>
-            <div>{selectedTv?.type}</div>
-            <span>|</span>
-            <div>{selectedTv?.genre}</div>
-            <span>|</span>
-            <div>{selectedTv?.language}</div>
-          </Stack>
+        <Stack direction='column' spacing={3} className='text-white'>
+          <Stack direction='column' spacing={1}>
+            <div className='text-3xl'>{selectedTv?.title}</div>
+            <Stack direction='row' spacing={2}>
+              <div>{selectedTv?.rating}</div>
+              <span>|</span>
+              <div> {selectedTv?.year}</div>
+              <span>|</span>
+              <div>{selectedTv?.type}</div>
+              <span>|</span>
+              <div>{selectedTv?.genre}</div>
+              <span>|</span>
+              <div>{selectedTv?.language}</div>
+            </Stack>
+          </Stack>{' '}
           <div>{selectedTv?.description}</div>
         </Stack>
       </Stack>
